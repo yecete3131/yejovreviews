17441fc08d42b241b420bb91bc7fefa6d5a107a8
yecete3131 <yusoup27@gmail.com>
2026-07-01 18:15:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 const params = new URLSearchParams(window.location.search);
 
-const business = params.get("business") || "demo";
+const business = params.get("business");
+
+document.getElementById("businessName").innerText =
+"Searching: " + business;
 
 
 fetch("./businesses.json")
 .then(response => response.json())
 .then(data => {
 
-    console.log(data);
+console.log("DATABASE:", data);
+console.log("REQUESTED:", business);
 
-    const b = data[business];
+
+const b = data[business];
 
 
-    document.getElementById("businessName").innerText = b.name;
+if(!b){
 
-    document.getElementById("tagline").innerText = b.tagline;
+document.getElementById("businessName").innerText =
+"Business not found: " + business;
 
-    document.getElementById("google").href = b.google;
+return;
 
-    document.getElementById("instagram").href = b.instagram;
+}
 
-    document.getElementById("facebook").href = b.facebook;
 
-    document.getElementById("website").href = b.website;
+document.getElementById("businessName").innerText = b.name;
 
-    document.getElementById("whatsapp").href = b.whatsapp;
+document.getElementById("tagline").innerText = b.tagline;
+
+document.getElementById("google").href = b.google;
+
+document.getElementById("instagram").href = b.instagram;
+
+document.getElementById("facebook").href = b.facebook;
+
+document.getElementById("website").href = b.website;
+
+document.getElementById("whatsapp").href = b.whatsapp;
 
 
 })
-.catch(error => {
+.catch(error=>{
 
 document.getElementById("businessName").innerText =
 "ERROR";
