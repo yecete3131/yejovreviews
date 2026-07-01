b616a95d64731c6302aeab07f7e8e4fe770f1464
yecete3131 <yusoup27@gmail.com>
2026-07-01 17:12:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const params =
+new URLSearchParams(window.location.search);
+
+
+const business =
+params.get("business") || "demo";
+
+
+fetch("businesses.json")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+
+const b=data[business];
+
+
+document.getElementById("businessName").innerHTML=b.name;
+
+
+document.getElementById("tagline").innerHTML=b.tagline;
+
+
+document.getElementById("logo").src=b.logo;
+
+
+document.getElementById("google").href=b.google;
+
+
+document.getElementById("instagram").href=b.instagram;
+
+
+document.getElementById("facebook").href=b.facebook;
+
+
+document.getElementById("website").href=b.website;
+
+
+document.getElementById("whatsapp").href=b.whatsapp;
+
+
+
+});
