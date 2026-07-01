6208892668e15c0c57119227ea8b8a4a7a17a19f
yecete3131 <yusoup27@gmail.com>
2026-07-01 17:28:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,45 @@ document.getElementById("whatsapp").href=b.whatsapp;
 
 
 });
+const params =
+new URLSearchParams(window.location.search);
+
+
+const business =
+params.get("business") || "demo";
+
+
+fetch("businesses.json")
+
+.then(res => res.json())
+
+.then(data => {
+
+
+const b = data[business];
+
+
+document.getElementById("businessName").innerText = b.name;
+
+document.getElementById("tagline").innerText = b.tagline;
+
+
+document.getElementById("google").href = b.google;
+
+document.getElementById("instagram").href = b.instagram;
+
+document.getElementById("facebook").href = b.facebook;
+
+document.getElementById("website").href = b.website;
+
+document.getElementById("whatsapp").href = b.whatsapp;
+
+
+})
+.catch(error=>{
+
+document.getElementById("businessName").innerText="YeJovReviews";
+
+document.getElementById("tagline").innerText="Setup error";
+
+});
